e18c3d6404cbf68a01b9002dc7f9a660e01b673c
jim9112 <jim9112@gmail.com>
2018-07-20 11:39:56 -0400

    adjusted payment functionality

--- js/app.js
+++ js/app.js
@@ -9,6 +9,10 @@ const design = document.getElementById('design');
 const colors = document.getElementById('color').children;
 const colorDiv = document.getElementById('color').parentElement;
 const activities = document.querySelector('.activities').children;
+const paymentOptions = document.getElementById('payment');
+const paypal = document.getElementById('paypal');
+const bitcoin = document.getElementById('bitcoin');
+const credit = document.getElementById('credit-card');
 
 
 // hides other text input box
@@ -27,7 +31,7 @@ for (let i = 0; i < colors.length; i++ ){
     let options = color[i];
     options.style.display = 'none';
 }
-//  hide color choice
+//  hide color choice selector
 colorDiv.style.display = 'none';
 
 // adjusts shirt color options based on style selection
@@ -54,5 +58,26 @@ design.addEventListener('change', (e) => {
             }
         }
         colorDiv.style.display = '';
+    }
+});
+
+// hide all payment options until one is selected
+paypal.style.display = 'none';
+bitcoin.style.display = 'none';
+
+// event listener for payment options and only display selected payment option
+paymentOptions.addEventListener('change', (e) => {
+    if(e.target.value === 'credit card'){
+        paypal.style.display = 'none';
+        bitcoin.style.display = 'none';
+        credit.style.display = '';
+    } else if(e.target.value === 'paypal'){
+        paypal.style.display = '';
+        bitcoin.style.display = 'none';
+        credit.style.display = 'none';
+    } else if(e.target.value === 'bitcoin'){
+        paypal.style.display = 'none';
+        bitcoin.style.display = '';
+        credit.style.display = 'none';
     }
 });
