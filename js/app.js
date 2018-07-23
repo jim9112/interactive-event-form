071f4c8830a45a1944cfb3c2852594e132e0f1d4
jim9112 <jim9112@gmail.com>
2018-07-23 12:53:48 -0400

    added totals and checkbox management for events

--- js/app.js
+++ js/app.js
@@ -8,13 +8,14 @@ const otherText = document.createElement('input');
 const design = document.getElementById('design');
 const colors = document.getElementById('color').children;
 const colorDiv = document.getElementById('color').parentElement;
-const activities = document.querySelector('.activities').children;
+const activities = document.querySelector('.activities');
 const paymentOptions = document.getElementById('payment');
 const paypal = document.getElementById('paypal');
 const bitcoin = document.getElementById('bitcoin');
 const credit = document.getElementById('credit-card');
+const individualActivities = document.querySelectorAll('.activities label input');
 
-
+console.log(individualActivities);
 // hides other text input box
 other.style.display = 'none';
 
@@ -60,6 +61,91 @@ design.addEventListener('change', (e) => {
         colorDiv.style.display = '';
     }
 });
+
+var totalPrice = 0;
+const totalText = document.createElement('p');
+totalText.id = 'eventTotal';
+activities.appendChild(totalText);
+
+const printTotal = () => {
+    const newTotal = document.getElementById('eventTotal');
+    newTotal.textContent = 'Total: $' + totalPrice;
+};
+
+// event listener for the event selection menu
+activities.addEventListener('change', (e) => {
+    // const checked = e.target.checked = true;
+    const selection = e.target;
+    const label = selection.parentElement;
+
+     // disable events of the same time slot
+    const disableWhenChecked = () => {
+        const activityLabels = document.querySelectorAll('.activities label');
+        if (label.textContent.includes('Tuesday 9am-12pm')){
+            for(let i = 0; i < activityLabels.length; i++){
+                const activityCheck = activityLabels[i];
+                if (activityCheck.textContent.includes('Tuesday 9am-12pm') && activityCheck.firstChild.checked != true) {
+                    activityCheck.firstChild.disabled = true;
+                }
+            }
+        } else if (label.textContent.includes('Tuesday 1pm-4pm')){
+            for(let i = 0; i < activityLabels.length; i++){
+                const activityCheck = activityLabels[i];
+                if (activityCheck.textContent.includes('Tuesday 1pm-4pm') && activityCheck.firstChild.checked != true) {
+                    activityCheck.firstChild.disabled = true;
+                }
+            }
+        }
+    };
+
+    // enable fields when unchecked
+    const enableWhenUnchecked = () => {
+        const activityLabels = document.querySelectorAll('.activities label');
+        if (label.textContent.includes('Tuesday 9am-12pm')){
+            for(let i = 0; i < activityLabels.length; i++){
+                const activityCheck = activityLabels[i];
+                if (activityCheck.textContent.includes('Tuesday 9am-12pm') && activityCheck.firstChild.checked != true) {
+                    activityCheck.firstChild.disabled = false;
+                }
+            }
+        } else if (label.textContent.includes('Tuesday 1pm-4pm')){
+            for(let i = 0; i < activityLabels.length; i++){
+                const activityCheck = activityLabels[i];
+                if (activityCheck.textContent.includes('Tuesday 1pm-4pm') && activityCheck.firstChild.checked != true) {
+                    activityCheck.firstChild.disabled = false;
+                }
+            }
+        }
+    };
+
+
+    // adjust the totals for event costs
+    if (selection.checked === true){
+        console.log(label.textContent);
+        if (label.textContent.includes("200")) {
+            totalPrice += 200;
+            printTotal();
+            disableWhenChecked();
+        } else if (label.textContent.includes("100")) {
+            totalPrice += 100;
+            printTotal();
+            disableWhenChecked();
+        }
+    } else if (selection.checked != true){
+        console.log(label.textContent);
+        if (label.textContent.includes("200")) {
+            totalPrice -= 200;
+            printTotal();
+            enableWhenUnchecked();
+        } else if (label.textContent.includes("100")) {
+            totalPrice -= 100;
+            printTotal();
+            enableWhenUnchecked();
+        }
+    }
+});
+
+
 
 // hide all payment options until one is selected
 paypal.style.display = 'none';
